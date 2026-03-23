2c9684bd03516df9b4615abb4d95c9a5623331a4
Vizio0 <lashachinchaladze2@gmail.com>
2026-03-23 17:51:52 +0400

    commit

---
+++ level 14/cw14.py
@@ -0,0 +1,7 @@
+for i in range(100):
+    print("Lasha")
+    print("Chinchaladze")
+
+
+for i in range(30, 50):
+    print(i)
---
+++ level 14/hw14.py
@@ -0,0 +1,29 @@
+#iteracia hqvia moqmedebis bevrjer gameoreebas
+#tu gvinda rom igive ram gavaketot bevrjer, unda gamoviyenot iteracia
+#algoritmi pythonshi ewodeba instruqciebs romlebsac nabij nabij mivuyvebit 
+
+
+#for loop 25 jer
+for i in range(25):
+    print("for loop")
+#25 dan 75 chatvlit
+for i in range(25, 76):
+    print(i)
+
+
+ #40 dan 90 mde
+for i in range(40, 90):
+    print(i)
+
+#4 is gamotovebit
+for i in range(25, 151, 4):
+    print(i)
+
+#10 gamotoveebit
+for i in range(100, 1001, 10):
+    print(i)
+
+
+#100jer python is cool
+for i in range(1, 101):
+    print(i, "python is cool")
