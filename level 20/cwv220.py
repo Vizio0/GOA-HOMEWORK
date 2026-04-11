1c2f2eb96c9b5e31d8d0469dee3f405d8278cd36
Vizio0 <lashachinchaladze2@gmail.com>
2026-04-11 23:13:30 +0400

    commit

---
+++ level 20/cwv220.py
@@ -0,0 +1,8 @@
+
+movies = ["GRAVITY FALLS", "AVATAR", "THEHUMANCENTIPIDE", "Matrix", "Hello Kitty", "LOONEYTUNESTURAGACA"]
+
+
+if movies[4] == "Hello Kitty":
+    movies[4] = "CAPTAINUNDERPANTS"  
+
+print(movies)
---
+++ level 20/hw.py
@@ -0,0 +1,72 @@
+# 1) favorite colors
+colors = ["black", "white", "pink"]
+
+# 2) weekdays
+days = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturady", "sunday"]
+
+# 3) 5 randdom numbers
+numbers = [17, 25, 7, 23, 10]
+print(numbers[3])
+
+# 4) 5 rrandom city/town
+cities = ["tbilisi", "rustavi", "zestafoni", "axalcixe", "batumi"]
+cities[2]="qobuleti"
+print(cities)
+
+# 5)
+family =["Lasha", "Mari", "Teona", "Jaba"]
+print(family[0])
+print(family[3])
+
+
+# 6) 
+numbers = [5, 12, 84, 70, 49, 10]
+
+numbers[4] = 100
+
+print(numbers)
+
+# 7)
+dogs =["Lexi", "bubba", "tobi", "buck", "bud", "bruno", "max", "coco" "rex", "lucy"]
+dogs[4] = "leo" 
+print(dogs)
+
+
+# 8)
+mixed_things = ["apple", "banana", "cherry", 10, 25, 7, True, False, True]
+
+print(mixed_things[2]) 
+
+
+# 9) 
+musicians = ["Drake", "Eminem", "Rihanna", "kingvon", "The Weeknd"]
+
+
+if musicians[0] == "Drake":
+    musicians[0] = "Kanye West"
+
+print(musicians)
+
+
+# 10) 
+numbers = [10, 5.5, 20, 3.14, 7]
+
+
+numbers[0] = numbers[0] + 1000
+numbers[4] = numbers[4] + 1000
+
+print(numbers)
+
+
+# 11)
+nums = [12, 2.5, 4]
+
+if nums[0] > 10:  
+    nums[2] = nums[2] + (nums[0] * nums[1])
+else:
+    nums[0] = nums[0] + (nums[1] * nums[2])
+
+print(nums)
+
+
+
