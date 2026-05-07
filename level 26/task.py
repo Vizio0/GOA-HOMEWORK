ab7f85570fafd36a3ec6e87b0fa7d7119e41697b
Vizio0 <lashachinchaladze2@gmail.com>
2026-05-07 19:07:48 +0400

    commit

---
+++ level 26/task.py
@@ -0,0 +1,22 @@
+#append aris tegi romelic siashi gvadzlevs sashualebas shevcvalot rame
+numbers = []
+numbers.append(3)
+numbers.append(7)
+numbers.append(1)
+numbers.append(9)
+numbers.append(5)
+
+print(numbers)
+
+
+words = ["apple", "blueberry", "cherry", "grapefruit"]
+words.remove("apple")
+
+print(words)
+
+nums = [10, 20, 30, 40, 50]
+
+removed_element = nums.pop(1)
+
+print("removed element:", removed_element)
+print("new list:", nums)
